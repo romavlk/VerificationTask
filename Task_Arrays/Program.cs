89877c14c351faeb58c134f08076072a9c75be68
Roman <roman_vlk@mail.ru>
2022-11-14 23:18:55 +0300

    task added

---
+++ Task_Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+Console.Clear();
+
+string[] arr1 = new string[5] { "Mon", "2022", "day", "14", "November" };
+string[] arr2 = new string[arr1.Length];
+void SecondArraySort(string[] arr1, string[] arr2)
+{
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[j] = arr1[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArraySort(arr1, arr2);
+PrintArray(arr2);
